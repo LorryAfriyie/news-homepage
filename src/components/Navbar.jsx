fba45e7f416b46b09aa69499f3ba76a1d68e1af7
Lawrence Afriyie <lawrenceafriyie35@gmail.com>
2024-04-25 04:19:53 +0200

    Sidebar functionality and overlay
    Setting and overlay when the hamburger menu is opened.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 
 // Navbar link items
 const navLinks = [
@@ -12,16 +12,47 @@ const navLinks = [
 export const Navbar = () => {
   // Used to access the navbar menu DOM
   const menu = useRef(null);
+  const closeBtn = useRef(null);
+  const openBtn = useRef(null);
+  const body = document.querySelector("body");
+  const media = window.matchMedia("(width < 40em)");
+
+  function setupTopNav(e) {
+    if (e.matches) {
+      console.log("mobile");
+      menu.current.setAttribute("inert", "");
+      menu.current.style.transition = "none";
+    } else {
+      console.log("desktop/tablet");
+      menu.current.removeAttribute("inert");
+      closeMenu();
+    }
+  }
 
   // Hamburger icon to open the menu
   const openMenu = () => {
-    menu.current.style.width = "270px";
+    openBtn.current.setAttribute("aria-expanded", "true");
+    menu.current.removeAttribute("inert");
+    menu.current.removeAttribute("style");
+    closeBtn.current.focus();
+    body.classList.add("overlay");
   };
 
   // Times icon to close the menu
   const closeMenu = () => {
-    menu.current.style.width = "0";
+    openBtn.current.setAttribute("aria-expanded", "false");
+    menu.current.setAttribute("inert", "");
+    closeBtn.current.focus();
+    body.classList.remove("overlay");
+
+    setTimeout(() => {
+      menu.current.style.transition = "none";
+    }, 500);
   };
+
+  useEffect(() => {
+    setupTopNav(media);
+  }, [media]);
 
   return (
     <nav className="navbar">
@@ -31,26 +62,39 @@ export const Navbar = () => {
       </div>
 
       {/* Hamburger icon */}
+      <span id="nav-label" hidden>
+        Navigation
+      </span>
       <button
         className="navbar__menu-button"
         onClick={openMenu}
-        width={"28px"}
-        height={"27px"}
+        ref={openBtn}
         aria-expanded={"false"}
+        aria-labelledby="nav-label"
       >
-        <img src="./images/icon-menu.svg" alt="icon-menu.svg" />
+        <img
+          src="./images/icon-menu.svg"
+          alt="icon-menu.svg"
+          width={"40px"}
+          height={"24px"}
+        />
       </button>
 
       {/* Navigation menu */}
-      <div className="navbar__navbar-menu" ref={menu}>
+      <div
+        className="navbar__navbar-menu"
+        ref={menu}
+        role="dialog"
+        aria-labelledby="nav-label"
+      >
         {/* Times icon */}
-        <button className="navbar__close-btn" onClick={closeMenu}>
-          <img
-            src="./images/icon-menu-close.svg"
-            alt="icon-menu-close.svg"
-            width={"28px"}
-            height={"27px"}
-          />
+        <button
+          className="navbar__close-btn"
+          onClick={closeMenu}
+          ref={closeBtn}
+          aria-label="close"
+        >
+          <img src="./images/icon-menu-close.svg" alt="icon-menu-close.svg" />
         </button>
 
         {/* Navigation link list items */}
