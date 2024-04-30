a3e3738f7c88f71deecdc77086ccf007ee805a8f
Lawrence Afriyie <lawrenceafriyie35@gmail.com>
2024-04-30 05:01:56 +0200

    Iterating through news data and styles
    Reading through news data from an array of objects and styling data appearance.

--- src/views/NewsPage.jsx
+++ src/views/NewsPage.jsx
@@ -1,16 +1,20 @@
 import { Navbar } from "../components/Navbar";
 import { Headline } from "./Headline";
 import { NewNews } from "./NewNews";
+import { TopNews } from "./TopNews";
 
 export const NewsPage = () => {
   return (
     <main className="news-page-section">
       <Navbar />
 
+      {/* Wrapper for headline and new news component */}
       <div className="wrapper">
         <Headline />
         <NewNews />
       </div>
+
+      <TopNews />
     </main>
   );
 };
---
+++ src/views/TopNews.jsx
@@ -0,0 +1,48 @@
+import img1 from "/images/image-gaming-growth.jpg";
+import img2 from "/images/image-top-laptops.jpg";
+import img3 from "/images/image-gaming-growth.jpg";
+
+export const TopNews = () => {
+  return (
+    <section className="top-news">
+      <div className="top-news-grid">
+        {topNewsData.map((x) => {
+          return (
+            <div key={x.id} className="info-grid">
+              <div className="picture">
+                <img src={x.img} alt={x.img} />
+              </div>
+              
+              <div className="news-details">
+                <h2>0{x.id}</h2>
+                <h2>{x.title}</h2>
+                <p>{x.post}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+const topNewsData = [
+  {
+    id: 1,
+    title: "Reviving Retro PCs",
+    post: "What happens when old PCs are given modern upgrades?",
+    img: img1,
+  },
+  {
+    id: 2,
+    title: "Top 10 Laptops of 2022",
+    post: "Our best picks for various needs and budgets.",
+    img: img2,
+  },
+  {
+    id: 3,
+    title: "The Growth of Gaming",
+    post: "How the pandemic has sparked fresh opportunities.",
+    img: img3,
+  },
+];
